5ab80ca06f59aee11c9d75601b726990e3c7db99
Andrew Duthie <aduthie7@gmail.com>
2013-01-22 21:18:49 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# grunt-jquerymanifest
+
+*Requires Grunt 0.4.0 or newer*
+
+Generate jQuery plugin manifest automatically from package.json values
+
+## Getting Started
+
+If you haven't used [grunt][http://gruntjs.com/] before, be sure to check out the [Getting Started][https://github.com/gruntjs/grunt/blob/devel/docs/getting_started.md] guide, as it explains how to create a gruntfile as well as install and use grunt plugins. Once you're familiar with that process, install this plugin with this command:
+
+```shell
+npm install grunt-jquerymanifest --save-dev
+```
+
+## Usage
+
+Below is an example gruntfile configuration:
+
+```javascript
+grunt.initConfig({
+    jquerymanifest: {
+        options: {
+            source: grunt.file.readJSON('package.json'),
+            overrides: {
+                name: "myPlugin",
+                title: "jquery.myPlugin",
+                author: {
+                    name: "Andrew Duthie",
+                    email: "andrew@andrewduthie.com",
+                    url: "http://www.andrewduthie.com"
+                },
+                homepage: "http://github.com/aduth/grunt-jquerymanifest",
+                demo: "http://github.com/aduth/grunt-jquerymanifest"
+            }
+        }
+    }
+});
+
+grunt.loadNpmTasks('grunt-jquerymanifest');
+
+grunt.registerTask('default', ['jquerymanifest']);
+
+```
+
+## Automatic Mappings
+
+The following manifest properties are automatically mapped from package.json:
+
+* `title` - Uses package.json `name`
+* `author` - Generates proper jQuery `people` object with package.json `name` as name
+* `licenses` - Generates proper jQuery `licenses` array with single entry using package.json `license` as type
+* `dependencies` - Returns object with jQuery ("*" version) as sole dependency
+
+## Options
+
+* `source` - This will typically be your package.json file. Specify either with a string (i.e. `"package.json"`) or the object itself (i.e. `grunt.file.readJSON('package.json')`)
+* `overrides` - An object containing custom values which will override any inferred value. For example, you might want to specify a `homepage` or `demo` URL here, since these are usually not defined as package.json properties
+
+## License
+
+Copyright (c) 2013 Andrew Duthie
+
+Released under the MIT License (see LICENSE.txt)
---
+++ tasks/jquerymanifest.js
@@ -0,0 +1,176 @@
+/*!
+ * grunt-jquerymanifest
+ * http://github.com/aduth/grunt-jquerymanifest
+ *
+ * Copyright (c) 2013 Andrew Duthie
+ * Licensed under the MIT License
+ */
+
+module.exports = function(grunt) {
+
+    require('string-format');
+
+    /**
+     * jQuery manifest properties configuration
+     *
+     * @constant
+     */
+    var fields = {
+        name: {
+            required: true
+        },
+        version: {
+            required: true
+        },
+        title: {
+            required: true,
+            mapping: function(source) {
+                return source.name;
+            }
+        },
+        author: {
+            required: true,
+            mapping: function(source) {
+                return {
+                    name: source.author
+                };
+            }
+        },
+        licenses: {
+            required: true,
+            mapping: function(source) {
+                return [{
+                    name: source.license
+                }];
+            }
+        },
+        dependencies: {
+            required: true,
+            mapping: function() {
+                return {
+                    'jquery': '*'
+                };
+            }
+        }
+    };
+
+    // Add optional properties
+    ['description', 'keywords', 'homepage', 'docs', 'demo', 'download', 'bugs', 'maintainers'].forEach(function(optionalField) {
+        fields[optionalField] = {
+            required: false
+        };
+    });
+
+    /**
+     * Static, formattable texts
+     *
+     * @constant
+     */
+    var texts = {
+        cannotReadSource: 'Cannot read source file',
+        missingRequiredField: 'Missing required field: `{0}`',
+        outputFile: '{0}.jquery.json'
+    };
+
+    /**
+     * jQuery manifest helper responsible for parsing package.json, generating output, and writing manifest
+     *
+     * @class
+     * @property {Object} options The grunt options object
+     */
+    var JqueryManifest = function(options) {
+        this.options = options;
+
+        /**
+         * Cached boolean indicating whether override values exist
+         *
+         * @type {Boolean}
+         */
+        this.hasOverrides = typeof options.overrides !== 'undefined';
+    };
+
+    /**
+     * Using options.source (which should point to package.json), return parsed object set of source values
+     *
+     * @returns {Object} The set of package.json source values
+     */
+    JqueryManifest.prototype.getSourceValues = function() {
+        if (typeof this.srcValues === 'undefined') {
+            switch (typeof this.options.source) {
+                case 'string':
+                    this.srcValues = grunt.file.readJSON(this.options.source);
+                    break;
+                case 'object':
+                    this.srcValues = this.options.source;
+                    break;
+                default:
+                    grunt.fail.warn(texts.cannotReadSource);
+            }
+        }
+
+        return this.srcValues;
+    };
+
+    /**
+     * Generate valid jQuery manifest object using source values
+     *
+     * @return {Object} A jQuery manifest object
+     */
+    JqueryManifest.prototype.getOutputValues = function() {
+        if (typeof this.destValues === 'undefined') {
+            var srcValues = this.getSourceValues(),
+                destValues = {},
+                _this = this;
+
+            Object.keys(fields).forEach(function(field) {
+                var fieldOpts = fields[field],
+                    destValue;
+
+                if (_this.hasOverrides && field in _this.options.overrides) {
+                    destValue = _this.options.overrides[field];
+                } else {
+                    if (typeof fieldOpts.mapping === 'function') {
+                        destValue = fieldOpts.mapping(srcValues);
+                    } else {
+                        destValue = srcValues[field];
+                    }
+                }
+
+                if (typeof destValue === 'undefined') {
+                    if (fieldOpts.required) {
+                        grunt.fail.warn(texts.missingRequiredField.format(field));
+                    }
+                } else {
+                    destValues[field] = destValue;
+                }
+            });
+
+            this.destValues = destValues;
+        }
+
+        return this.destValues;
+    };
+
+    /**
+     * Writes [name].jquery.json manifest file, where name is pulled from manifest object
+     *
+     * @return {Boolean} True if file write was successful, false otherwise
+     */
+    JqueryManifest.prototype.writeManifest = function() {
+        var destValues = this.getOutputValues();
+
+        return grunt.file.write(
+            texts.outputFile.format(destValues.name),
+            JSON.stringify(destValues, null, 2)
+        );
+    };
+
+    // Register grunt task
+    grunt.registerTask(
+        'jquerymanifest',
+        'jQuery manifest helper responsible for parsing package.json, generating output, and writing plugin manifest',
+        function() {
+            return new JqueryManifest(this.options()).writeManifest();
+        }
+    );
+};
